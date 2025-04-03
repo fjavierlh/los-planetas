a2af0b54e7daf834afa773acbfd69a8198e62cbf
Javier López <fjavierlh@gmail.com>
2025-04-04 00:53:14 +0200

    refactor: remove useless config

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,4 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/los-planetas/",
-  build: {
-    rollupOptions: {
-      external: ["@mediapipe/face_mesh", "@tensorflow/tfjs"]
-    }
-  },
-  optimizeDeps: {
-    include: ["@mediapipe/face_mesh", "@tensorflow/tfjs"]
-  }
 })
